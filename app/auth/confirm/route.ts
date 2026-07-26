653141fba3d35ce7d64b1f5babb7d8b8544a4b3f
burkhardtmarkus93 <burkhardt.markus93@gmail.com>
2026-07-27 01:33:04 +0200

    Enhance GET route for code and token verification (#5)

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -3,11 +3,33 @@ import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url);
+
   const code = searchParams.get("code");
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type");
+
+  const supabase = await createClient();
 
   if (code) {
-    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      return NextResponse.redirect(`${origin}/update-password`);
+    }
+  }
+
+  if (token_hash && type) {
+    const { error } = await supabase.auth.verifyOtp({
+      token_hash,
+      type: type as
+        | "signup"
+        | "invite"
+        | "magiclink"
+        | "recovery"
+        | "email_change"
+        | "email",
+    });
+
     if (!error) {
       return NextResponse.redirect(`${origin}/update-password`);
     }
