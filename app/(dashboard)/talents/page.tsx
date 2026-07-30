4a9c10253abe3e3d3a08d5d279b6e1fcc8695790
burkhardtmarkus93 <burkhardt.markus93@gmail.com>
2026-07-31 01:39:48 +0200

    Simplify TalentsPage component structure (#34)
    Removed Link and Button components from TalentsPage.

--- app/(dashboard)/talents/page.tsx
+++ app/(dashboard)/talents/page.tsx
@@ -1,14 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/Button";
-
-export default async function TalentsPage() {
-  return (
-    <div>
-      <h1>Talente</h1>
-      <Link href="/talents/new">
-        <Button>+ Neues Talent</Button>
-      </Link>
-      <p>Minimaltest</p>
-    </div>
-  );
+export default function TalentsPage() {
+  return <div>Minimaltest</div>;
 }
