8a9cf92ab05171d2320fde42f36972265ca7a207
burkhardtmarkus93 <burkhardt.markus93@gmail.com>
2026-07-31 01:41:32 +0200

    Update TalentsPage component layout and content

--- app/(dashboard)/talents/page.tsx
+++ app/(dashboard)/talents/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function TalentsPage() {
-  return <div>Minimaltest</div>;
+  return (
+    <div>
+      <h1>Talente</h1>
+      <Link href="/talents/new">+ Neues Talent</Link>
+    </div>
+  );
 }
