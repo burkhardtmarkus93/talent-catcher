439e8e50d1c9f8c2a24bea5c40a7d9f5cadba29a
burkhardtmarkus93 <burkhardt.markus93@gmail.com>
2026-07-31 01:45:39 +0200

    Replace link with button for new talent navigation

--- app/(dashboard)/talents/page.tsx
+++ app/(dashboard)/talents/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/Button";
 
 export default function TalentsPage() {
   return (
     <div>
       <h1>Talente</h1>
-      <Link href="/talents/new">+ Neues Talent</Link>
+      <Link href="/talents/new">
+        <Button>+ Neues Talent</Button>
+      </Link>
     </div>
   );
 }
